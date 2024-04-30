096e75aa100023cf8e51a6144a6d917df1bad6b5
Ylenia1 <yle604501@gmail.com>
2024-04-30 15:40:20 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@
     <Navbar />
     <router-view/>
     <Jumbotron />
+    <Features />
+    <frecciaSu />
+    <nuovazona />
+    <appFooter />
+ 
+
+
+
   </div>
 
 </template>
@@ -10,22 +18,38 @@
 <script>
 import Navbar from './components/Navbar.vue';
 import Jumbotron from './components/Jumbotron.vue';
+import frecciaSu from './components/frecciaSu.vue';
+import Features from './components/Features.vue'; 
+import appFooter from './components/appFooter.vue';/*
+import nuovazona from './components/nuovazona.vue';*/
+
+
+
 
 export default {
   name: 'App',
   components: {
     Navbar,
-    Jumbotron 
-  }
-}
+    Jumbotron,
+    frecciaSu,
+    Features,/*
+    nuovazona,*/
+    appFooter,
+ 
+
+  }  }
+
 </script>
+
+
 <style>
+
 html body {
   font-family: 'Montserrat','Poppins', 'sans-serif';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  background-color: #212529; /* Nero */
+  background-color: #212529; 
   margin-top: 40px;
 }
 
